1cb57b8eeaadd661d79f60f35cd34b79422ab476
denysb23 <75504352+denysb23@users.noreply.github.com>
2023-12-05 10:34:48 +0000

    implemented forward pass for the encoder

--- main.py
+++ main.py
@@ -64,7 +64,6 @@ class AddNorm(nn.Module):
         return self.layer_norm(X + Y)
 
 
-#for feedforward layer add bias
 class FeedForward(nn.Module):
     def __init__(self, d_model, d_ff, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -77,6 +76,25 @@ class FeedForward(nn.Module):
 
     def forward(self, X):
         return self.linear_2(torch.relu(self.linear_1(X) + self.b1)) + self.b2
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, d_k, d_v, h, d_ff, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.d_model = d_model
+        self.h = h
+        self.dk = d_k
+        self.dv = d_v
+        self.dff = d_ff
+        self.multi_head_attention = MultiHeadAttention(h, d_model, d_k, d_v)
+        self.add_norm = AddNorm(d_model)
+        self.feed_forward = FeedForward(d_model, d_ff)
+
+    def forward(self, X):
+        Y = MultiHeadAttention(self.h, self.d_model, self.d_k, self.d_v).forward(X, X, X)
+        Y = self.add_norm(X, Y)
+        Z = self.feed_forward.forward(Y)
+        return self.add_norm(Y, Z)
 
 
 
