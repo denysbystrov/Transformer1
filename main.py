b3d31c8c6cd4cf3fae6fd69c05e75052d1d4668b
denysb23 <75504352+denysb23@users.noreply.github.com>
2023-12-05 10:13:29 +0000

    implemented forward pass for Addition + Normalisation of sub-layers

--- main.py
+++ main.py
@@ -54,3 +54,15 @@ class MultiHeadAttention(nn.Module):
         return outputs
 
 
+class AddNorm(nn.Module):
+    def __init__(self, d_model, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.d_model = d_model
+        self.layer_norm = nn.LayerNorm(d_model)
+
+    def forward(self, X, Y):
+        return self.layer_norm(X + Y)
+
+
+
+
