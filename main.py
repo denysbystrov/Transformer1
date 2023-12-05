89e4fd261e646d356cb0dd9e9b8b6e0634eecc25
denysb23 <75504352+denysb23@users.noreply.github.com>
2023-12-05 10:22:53 +0000

    implemented forward pass for Feed-Forward network

--- main.py
+++ main.py
@@ -64,5 +64,20 @@ class AddNorm(nn.Module):
         return self.layer_norm(X + Y)
 
 
+#for feedforward layer add bias
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.d_model = d_model
+        self.d_ff = d_ff
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+        self.b1 = nn.Parameter(torch.zeros(d_ff))
+        self.b2 = nn.Parameter(torch.zeros(d_model))
+
+    def forward(self, X):
+        return self.linear_2(torch.relu(self.linear_1(X) + self.b1)) + self.b2
+
+
 
 
