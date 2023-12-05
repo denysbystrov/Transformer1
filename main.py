269160f6d7ffbecd23baf688ce88b1046a9ad3b7
denysb23 <75504352+denysb23@users.noreply.github.com>
2023-12-05 09:44:21 +0000

    implemented forward pass for multi-attention head

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+# implementation of a transformer model for sentence completion using pytorch
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import torch
+import torch.nn as nn
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class AttentionHead(nn.Module):
+    def __init__(self, d_model, d_k, d_v, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.W_Q = nn.Linear(d_model, d_k)
+        self.W_K = nn.Linear(d_model, d_k)
+        self.W_V = nn.Linear(d_model, d_v)
+        self.output = None
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def forward(self, Q, K, V):
+        Q = self.W_Q(Q)
+        K = self.W_K(K)
+        V = self.W_V(V)
+        # compute the scaled dot-product attention
+
+        SM = nn.Softmax(dim=2)
+        QK = torch.bmm(Q, K.transpose(1, 2))
+        QK = QK / torch.sqrt(torch.tensor(self.d_k, dtype=torch.float32))
+        QK = SM(QK)
+        self.output = torch.bmm(QK, V)
+        return self.output
+
+
+# implement the multi-head attention module
+class MultiHeadAttention(nn.Module):
+    def __init__(self, h, d_model, d_k, d_v, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.h = h
+        self.d_model = d_model
+        self.d_k = d_k
+        self.d_v = d_v
+        self.attention_heads = []
+        for i in range(h):
+            self.attention_heads.append(AttentionHead(d_model, d_k, d_v))
+        self.W_O = nn.Linear(h * d_v, d_model)
+
+    def forward(self, Q, K, V):
+        # concatenate the outputs of the attention heads into one matrix
+        # and apply a linear transformation to get the final output
+        outputs = []
+        for i in range(self.h):
+            self.attention_heads[i].forward(Q, K, V)
+            outputs.append(self.attention_heads[i].output)
+            outputs = torch.cat(outputs, dim=2)
+        outputs = self.W_O(outputs)
+        return outputs
+
+
